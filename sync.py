b8a8bf86c68c78adac729ab27c1b9d38595b19fc
Noah Huber-Feely <nhuberfeely@gmail.com>
2017-01-14 15:00:03 -0600

    Add addition and viewing features to script

--- sync.py
+++ sync.py
@@ -1,11 +1,27 @@
 import os
+import sys
 
 home = '/Users/noah/scripts/GIT_ER/'
 h = open(home + 'watchList.txt')
 d = h.read()
+h.close()
 paths = d.split('\n')
 
-for i in paths:
-    if not i == '':
-        print 'git push ', i
-        os.system('cd "' + i + '"; git push;')
+try:
+    if sys.argv[1] == 'add':
+        z = open(home + 'watchList.txt', 'w')
+        z.write(d + sys.argv[2] + '\n')
+    elif sys.argv[1] == 'show':
+        print d
+    elif sys.argv[1] == 'sync':
+        for i in paths:
+            if not i == '':
+                print 'git push ', i
+                os.system('cd "' + i + '"; git pull; git push;')
+    else:
+        print "Supplied argument found no match."
+except:
+    for i in paths:
+        if not i == '':
+            print 'git push ', i
+            os.system('cd "' + i + '"; git pull; git push;')
